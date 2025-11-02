dc2edfc852807783616389db6a69cbe5ccdb7b07
Shashank Devadiga <beast.steam002@gmail.com>
2025-11-02 17:27:02 +0530

    tailwind class update test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,10 +2,10 @@
 const Home = () => {
   return (
     <div className="py-20">
-      <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight ">
+      <h1 className="bg-clip-text text-transparent text-center bg-linear-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight ">
             Unleash the power of <br />
-            <span className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-5xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-              <span className="bg-gradient-to-b from-blue-400 to-blue-950 bg-clip-text text-transparent"> प्रश्न | </span>Prashn</span>
+            <span className="bg-clip-text text-transparent text-center bg-linear-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-5xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+              <span className="bg-linear-to-b from-blue-400 to-blue-950 bg-clip-text text-transparent"> प्रश्न | </span>Prashn</span>
           </h1>
     </div>
   )
